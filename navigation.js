2b93b642586ddf4b224f292e07575f1af254280c
nigelwhite <webdev@permaculture.org.uk>
2020-10-29 10:01:10 +0000

    active class gone

--- navigation.js
+++ navigation.js
@@ -5,3 +5,30 @@ $(function () {
 $(function () {
 	$('#navigation-drawer').load('navigation-drawer.html');
 });
+
+// $(function () {
+// 	$(function () {
+// 		var url = window.location.pathname,
+// 			urlRegExp = new RegExp(url.replace(/\/$/, '') + '$'); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
+// 		// now grab every link from the navigation
+// 		$('.menu').each(function () {
+// 			// and test its normalized href against the url pathname regexp
+// 			if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
+// 				$(this).addClass('uk-active');
+// 			}
+// 		});
+// 	});
+// });
+
+// $(function () {
+// 	var index = window.location.pathname.split('/')[1];
+// 	$('ul.li').removeClass('uk-active');
+// 	$('li.' + index).addClass('uk-active');
+// });
+
+$(function () {
+	$('ul li a').click(function () {
+		$('li').removeClass('uk-active');
+		$(this).addClass('uk-active');
+	});
+});
