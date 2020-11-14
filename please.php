72ed6c205e7fa51361cf985827dfdfa526bef7e7
nigelwhite <webdev@permaculture.org.uk>
2020-11-14 13:37:42 +0000

    triangle ok

--- please.php
+++ please.php
@@ -23,15 +23,30 @@ $perspective = htmlspecialchars($_POST['perspective']);
 $myHope = htmlspecialchars($_POST['myHope']);
 // assemble stuff
 $name = $fname.' '.$lname;
-$myDetails = '<p>My details<br/>
+$myDetails = '<p>My details are -<br/>
     Name: '.$name.'<br/>          
     Email: '.$email.'</p><hr/>';
+$joiningDetails = '<p>Note to admins: we are signing up a new person, so we want to be sure they are a genuine Headingley resident. You can tell by scanning the answers they give to these 4 questions. You do not need to check for accuracy - it is usually obvious.</p>
+    <p>My street: '.$street.'<br/>
+    My postcode: '.$postcode.'<br/>
+    My perspective: '.$perspective.'<br/>
+    My hope in joinng: '.$myHope.'</p>';
+$thanksAdmin = '<hr/><p>PS (from Nigel).<br/> Dear admin volunteer - Many thanks for your work for the community.</p>';
 // assemble chat
 $chatBody = '';
 if ($chatAction === 'subscribe Chat') {
-        $chatBody = '<h2>Please subscribe me to Chat</h2>'.$myDetails;
+        $chatBody = '<h2>Please subscribe me to Chat</h2>'.$myDetails.$joiningDetails.$thanksAdmin;
     } elseif ($chatAction === 'unsubscribe Chat') {
-        $chatBody = '<h2>Please unsubscribe me from Chat</h2>'.$myDetails;
+        $chatBody = '<h2>Please unsubscribe me from Chat</h2>'.$myDetails.$thanksAdmin;
+    } elseif ($chatAction === 'change email Chat') {
+        $chatBody = '<h2>Please change my email on Chat</h2>
+        <p>These are my current details -<br/>
+        Name: '.$name.'<br/>          
+        Email: '.$email.'</p>
+        <p>This is my old email: '.$oldEmail.'</p>'.$thanksAdmin;
+    } elseif ($chatAction === 'other Chat') {
+        $chatBody = '<h2>Please change my Chat settings as follows</h2>
+        <p>'.$chatDetails.'</p>'.$myDetails.$thanksAdmin;
     }
 
 
@@ -65,22 +80,22 @@ if ($chatAction === 'subscribe Chat') {
 
 
     // general stuff
-    echo 'Name: '.$fname.' '.$lname;
-    echo "<br />";
-    echo 'Email: '.$email;
-    echo "<br />";
-    echo 'Old email: '.$oldEmail;
-    echo "<br /><hr/>";
+    // echo 'Name: '.$fname.' '.$lname;
+    // echo "<br />";
+    // echo 'Email: '.$email;
+    // echo "<br />";
+    // echo 'Old email: '.$oldEmail;
+    // echo "<br /><hr/>";
     
     
-    // joining stuff
-    echo 'Street: '.$street;
-    echo "<br />";
-    echo 'Postcode: '.$postcode;
-    echo "<br />";
-    echo 'Perspective: '.$perspective;
-    echo "<br />";
-    echo 'My hope in joining: '.$myHope;
+    // // joining stuff
+    // echo 'Street: '.$street;
+    // echo "<br />";
+    // echo 'Postcode: '.$postcode;
+    // echo "<br />";
+    // echo 'Perspective: '.$perspective;
+    // echo "<br />";
+    // echo 'My hope in joining: '.$myHope;
     // redirect
     echo file_get_contents("thanks.html");
 
